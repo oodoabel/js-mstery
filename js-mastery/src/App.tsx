8c2f20a3c42fc35075abff493cb1c220d5fb9c83
oodoabel <get2abeloodo@gmail.com>
2024-09-22 22:00:56 +0100

    02 commit

--- js-mastery/src/App.tsx
+++ js-mastery/src/App.tsx
@@ -1,9 +1,28 @@
 import "./App.css";
 
+interface Props {
+  name: string;
+  name2: string;
+  age: number;
+}
+
+const Components = ({ name, name2, age }: Props) => {
+  return (
+    <>
+      <h1>firstName: {name}</h1>
+      <h2>lastName: {name2}</h2>
+      <h3>Age: {age}</h3>
+    </>
+  );
+};
+
 const App = () => {
   return (
     <>
-      <h1>Hello React!</h1>
+      <Components name="john" name2="doe" age={23} />
+      <Components name="mike" name2="doe" age={43} />
+      <Components name="mark" name2="doe" age={30} />
+      <Components name="james" name2="doe" age={2} />
     </>
   );
 };
